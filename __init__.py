70bb1c288ca810b93dbdee2a68a1a1d40301dd7e
Andy Ouyang <andy.ouyang@stonybrook.edu>
2019-03-23 16:48:06 -0400

    added getquestion

--- __init__.py
+++ __init__.py
@@ -33,8 +33,42 @@ class AddQuestion(Resource):
 		question['timestamp'] = time.time()
 		question['accepted_answer_id'] = None
 		question['media'] = []
+		question['viewed'] = []
 		questions.insert_one(question)
 		return {'status': 'OK', 'id': question['id']}
+
+class GetQuestion(Resource):
+	def post(self):
+		args = parse_args_list(['id', 'user'])
+		questions = get_questions_coll()
+		question = questions.find_one({'id':args['id']})
+		viewed = question['viewed']
+		inc = args['user'] not in viewed
+		if inc:
+			viewed.append(args['user'])
+			questions.update_one({'id':args['id']}, {'$push':{'viewed':args['user']}, 
+				'$set':{'view_count':len(viewed)}})
+		resp = {}
+		resp['status'] = 'OK'
+		resp['id'] = question['id']
+		resp['title'] = question['title']
+		resp['body'] = question['body']
+		resp['score'] = question['score']
+		resp['view_count'] = question['view_count'] if not inc else len(viewed)
+		resp['answer_count'] = question['answer_count']
+		resp['timestamp'] = question['timestamp']
+		resp['media'] = question['media']
+		resp['media'] = question['media']
+		resp['tags'] = question['tags']
+		resp['accepted_answer_id'] = question['accepted_answer_id']
+		users = get_users_coll()
+		user = users.find_one({'username':question['username']})
+		u = {}
+		u['username'] = user['username']
+		u['reputation'] = user['reputation']
+		resp['user'] = u
+		return resp
+
 
 def parse_args_list(argnames):
 	parser = reqparse.RequestParser()
@@ -49,8 +83,14 @@ def get_questions_coll():
 	users = mydb['questions']
 	return users
 
-api.add_resource(AddQuestion, '/add')
+def get_users_coll():
+	myclient = pymongo.MongoClient('mongodb://130.245.170.88:27017/')
+	mydb = myclient['finalproject']
+	users = mydb['users']
+	return users	
 
+api.add_resource(AddQuestion, '/add')
+api.add_resource(GetQuestion, '/getquestion')
 
 
 if __name__ == '__main__':
